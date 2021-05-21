29ce9d5cd6288059b8623e00b1a1d4a4fb3fbf23
Khusniyarovmr <Khusniyarovmr@gmail.com>
2021-05-21 19:52:03 +0300

    7 рабочий

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 bind = '0.0.0.0:8080'
 def application(env, start_response):
     response_body = [
-        bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')
+    	bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')
     ]
+    #response_body = '\n'.join(response_body)
     status = '200 OK'
     headers = [
     	('content-type', 'text/plain')
